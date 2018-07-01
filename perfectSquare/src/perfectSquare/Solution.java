3d4afed8cf8f4f1a0379ea261d262f703ad01671
wwz14 <wwz14@software.nju.edu.cn>
2018-07-01 23:41:58 +0800

    solve problem

--- perfectSquare/src/perfectSquare/Solution.java
+++ perfectSquare/src/perfectSquare/Solution.java
@@ -3,19 +3,19 @@ package perfectSquare;
 import java.util.Arrays;
 
 public class Solution {
-	public int numSquares(int n) {
-		int[] dp = new int[n + 1];
-		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[0] = 0;
-		for(int i = 1; i <= n; ++i) {
-			int min = Integer.MAX_VALUE;
-			int j = 1;
-			while(i - j*j >= 0) {
-				min = Math.min(min, dp[i - j*j] + 1);
-				++j;
-			}
-			dp[i] = min;
-		}		
-		return dp[n];
-	}
+    public int numSquares(int n) {
+        
+        int[] res = new int[n+1];
+        res[0] = 0;
+        res[1] = 1;
+        for(int i = 2;i<n+1;i++) {
+            res[i] = Integer.MAX_VALUE;
+            for(int j = 1;i-j*j>=0;j++){
+                res[i] = Math.min(res[i],res[i-j*j]+1);
+            }
+        }
+        
+        return res[n];
+        
+    }
 }
